885d9b583b2d851edc9f6c3ffbacc6d84023f617
coolCicada <1475395755@qq.com>
2024-10-12 11:50:36 +0800

    feat: doc 提成包

--- packages/simply-ui-docs/vite.config.ts
+++ packages/simply-ui-docs/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reads from './vite-plugins/reads';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    reads(path.resolve() + '/src')
   ]
 });
